6447644aad206e9d1b4f2a47f1318b693edc0e17
Musa <musa@iMac10.local>
2018-12-04 13:47:45 -0500

    created rectangles for the logo

--- unit_ten_warmup.py
+++ unit_ten_warmup.py
@@ -1,9 +1,14 @@
 import pygame, sys
 from pygame.locals import *
+import logo
+
 
 pygame.init()
-mainSurface = pygame.display.set_mode((500, 500), 0, 32)
+mainSurface = pygame.display.set_mode((500, 250), 0, 32)
 pygame.display.set_caption("Pygame Test Window")
+
+ssfs = logo.Logo(mainSurface)
+ssfs.draw_rectangles()
 
 while True:
     for event in pygame.event.get():
