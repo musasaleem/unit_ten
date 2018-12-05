e723d5e372bbe3a833e6e683020336625c54be7d
Musa <musa@iMac10.local>
2018-12-05 14:40:00 -0500

    colors

---
+++ cash.py
@@ -0,0 +1,7 @@
+        self.GREEN = (0, 255, 0)
+        self.GOLD = (255, 209, 63)
+        self.WHITE = (255, 255, 255)
+        self.YELLOW = (255, 255, 0)
+        self.RED = (255, 0, 0)
+        self.BLUE =(0, 0, 255)
+        self.PURPLE = (255, 0, 255)
