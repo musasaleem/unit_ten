aef8a8bcc85e641d4ef7eb61e71202cfdfb132aa
Musa <musa@iMac10.local>
2018-12-05 14:40:38 -0500

    making a brick

---
+++ brick.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Brick:
+    def __init__(self, width, height, color, main_surface):
+        self.main_surface = main_surface
+        self.color = color
+        self.height = height
+        self.width = width
+
+    def create_a_brick(self, x,y):
+        pygame.draw.rect(self.main_surface, self.color,(x, y, self.width, self.height), 0)
+        pygame.display.update()
