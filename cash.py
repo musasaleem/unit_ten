f926b2da15596bddf91871d7d652e4ff0e4fb232
Musa <musa@iMac10.local>
2018-12-10 10:15:18 -0500

    more work

--- cash.py
+++ cash.py
@@ -1,7 +1,36 @@
-        self.GREEN = (0, 255, 0)
-        self.GOLD = (255, 209, 63)
-        self.WHITE = (255, 255, 255)
-        self.YELLOW = (255, 255, 0)
-        self.RED = (255, 0, 0)
-        self.BLUE =(0, 0, 255)
-        self.PURPLE = (255, 0, 255)
+import brick
+import pygame, sys
+from pygame.locals import *
+
+
+GREEN = (0, 255, 0)
+GOLD = (255, 209, 63)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+BLUE =(0, 0, 255)
+PURPLE = (255, 0, 255)
+SPACE = 5
+HEIGHT = 20
+x_number_bricks = 9
+
+
+pygame.init()
+mainSurface = pygame.display.set_mode((500, 250), 0, 32)
+pygame.display.set_caption("Bricks")
+
+WIDTH = (500 - (x_number_bricks * SPACE))/x_number_bricks
+
+x = 0
+y = 250 - HEIGHT
+
+for b in range(x_number_bricks):
+    bricks = brick.Brick(WIDTH, HEIGHT, GREEN, mainSurface)
+    bricks.create_a_brick(x, y)
+    pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
